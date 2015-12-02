f2ba96cc987e54b356c1e42e2c225eb4b8ec9f2d
Jennifer Tigner <jennifertigner@email.com>
2015-12-02 01:58:02 +0000

    Added button to click and loads all contacts

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,17 @@
 get '/' do
   erb :index
 end
+
+get '/contacts' do
+  Contact.all.to_json
+end
+
+# post '/contacts' do
+#   results = {result: false}
+#   firstname = params[:firstname]
+#   lastname = params[:lastname]
+#   email = params[:email]
+
+#   contact = Contact.new
+#   //unfinished
+# end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,32 @@
-$(document).ready(function() {
+$(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#menu").on('click', function(e) {
+    e.preventDefault();
+    console.log("SUCCESS");
+    var temp = e.target.id;
+  });
+
+// $('#newContact').get()
+
+// $('#showAll')
+// $('#showOne')
+// $('#contactSearch')
+
+// for the first 3, specify the data type as JSON
+// $.get()
+// $.post()
+// $.ajax
+// $.getJSON()
+
+$('#showAll').on('click', function() {
+  $.getJSON("/contacts", function(contacts) {
+    var container = $('#people').find('tbody')       //fix this line
+    $.each(contacts, function(index, contact) {
+      var tr = $("<tr>").appendTo(container);
+      $("<td>").text(contact.id).appendTo(tr);
+      $("<td>").text(contact.firstname).appendTo(tr);
+      $("<td>").text(contact.lastnamename).appendTo(tr);
+      $("<td>").text(contact.email).appendTo(tr);
+    });
+  });
 });
